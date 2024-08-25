bc682ea1ccda5a8d70abb78bcc04bb48898c31da
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-08-25 12:09:13 +0800

    Add animation in login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -7,6 +7,8 @@ import { useState } from "react";
 import useGetUsers from "../hooks/useGetUsers";
 import { useStore } from "../zustand/store";
 import { toast } from "react-toastify";
+import Lottie from "lottie-react";
+import welcomeAnim from "../assets/Animation - 1707144264244 (1).json";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -34,55 +36,66 @@ const Login = () => {
 
   return (
     <AuthLayout>
-      <div
-        className="container mx-aut0 w-6/12 rounded-lg p-14"
-        style={{
-          background:
-            "linear-gradient(87deg, rgba(255,51,50,1) 0%, rgba(255,131,76,1) 100%)",
-        }}
-      >
-        <div className="flex justify-center items-center">
-          <h1 className="text-white font-bold text-2xl text-center uppercase">
-            Login to your web-based tournament management system
-          </h1>
-          <img src={logo} width={100} alt="" />
+      <div className="flex mx-20">
+        <div className="basis-6/12  flex justify-center items-center">
+          <Lottie
+            animationData={welcomeAnim}
+            style={{ width: 500 }}
+            loop={true}
+          />
         </div>
-
-        <TmsInput
-          onChange={(event) => setEmail(event.target.value)}
-          placeHolder="Enter your email"
-          label="Email"
-        />
-        <TmsInput
-          type={"password"}
-          onChange={(event) => setPassword(event.target.value)}
-          placeHolder="Enter your password"
-          label="Password"
-        />
-        <div className="flex justify-end items-center mt-2">
-          <Link className="text-blue-950 font-bold" to={"/forgot"}>
-            Forgot Password?
-          </Link>
-        </div>
-
-        <div className="flex justify-center items-center">
-          <Button
-            onClick={handleLogin}
-            className="w-8/12 mt-5 py-2"
-            gradientMonochrome="info"
+        <div className="basis-6/12 flex justify-center items-center ">
+          <div
+            className="container mx-auto rounded-lg p-14 mx-10"
+            style={{
+              background:
+                "linear-gradient(87deg, rgba(255,51,50,1) 0%, rgba(255,131,76,1) 100%)",
+            }}
           >
-            Login
-          </Button>
-        </div>
+            <div className="flex justify-center items-center">
+              <h1 className="text-white font-bold text-2xl text-left uppercase">
+                Login to your web-based tournament management system
+              </h1>
+              <img src={logo} width={100} alt="" />
+            </div>
 
-        <div className="flex justify-between items-center mt-5">
-          <div className="wrapper flex items-center "></div>
-          <div className="wrapper flex items-center ">
-            <p>Dont' have an account?</p>
+            <TmsInput
+              onChange={(event) => setEmail(event.target.value)}
+              placeHolder="Enter your email"
+              label="Email"
+            />
+            <TmsInput
+              type={"password"}
+              onChange={(event) => setPassword(event.target.value)}
+              placeHolder="Enter your password"
+              label="Password"
+            />
+            <div className="flex justify-end items-center mt-2">
+              <Link className="text-blue-950 font-bold" to={"/forgot"}>
+                Forgot Password?
+              </Link>
+            </div>
 
-            <Button size={"xs"} className="ml-2">
-              <Link to={"/registration"}> Create an account</Link>
-            </Button>
+            <div className="flex justify-center items-center">
+              <Button
+                onClick={handleLogin}
+                className="w-8/12 mt-5 py-2"
+                gradientMonochrome="info"
+              >
+                Login
+              </Button>
+            </div>
+
+            <div className="flex justify-between items-center mt-5">
+              <div className="wrapper flex items-center "></div>
+              <div className="wrapper flex items-center ">
+                <p>Dont' have an account?</p>
+
+                <Button size={"xs"} className="ml-2">
+                  <Link to={"/registration"}> Create an account</Link>
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
