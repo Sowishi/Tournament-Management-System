9303bd06044a4bcc2a9ac483a11b9faacee4cff8
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-10-07 03:13:03 +0800

    Add about content

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,6 +5,24 @@ const About = () => {
   return (
     <DefaultLayout>
       <Title title={"About us"} />
+      <div className="container mx-auto p-20">
+        <h1 className="text-4xl font-bold mb-5">Our Mission</h1>
+        <p className="text-lg">
+          Our mission is to revolutionize the way tournaments are organized by
+          providing a seamless, user-friendly platform that simplifies every
+          step of the process. We aim to empower organizers and participants
+          alike with tools that enhance the experience, foster competition, and
+          promote a sense of community.
+        </p>
+        <h1 className="text-4xl font-bold my-5">Our Our Vision</h1>
+        <p className="text-lg">
+          Our mission is to revolutionize the way tournaments are organized by
+          providing a seamless, user-friendly platform that simplifies every
+          step of the process. We aim to empower organizers and participants
+          alike with tools that enhance the experience, foster competition, and
+          promote a sense of community.
+        </p>
+      </div>
     </DefaultLayout>
   );
 };
