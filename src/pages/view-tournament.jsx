2e7c94f685c7ef8f1b550f5d4de4cc3ed6aa76b4
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-11-03 23:16:47 +0800

    change the layout of the tournaments

--- src/pages/view-tournament.jsx
+++ src/pages/view-tournament.jsx
@@ -13,6 +13,10 @@ import useCrudMatches from "../hooks/useCrudMatches";
 import MatchCard from "../components/matchCard";
 import moment from "moment";
 import DefaultLayout from "../layout/defaultLayout";
+import { FaTrophy } from "react-icons/fa";
+import { GiStairs } from "react-icons/gi";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { TbTournament } from "react-icons/tb";
 
 const ViewTournament = () => {
   const { id } = useParams();
@@ -181,137 +185,159 @@ const ViewTournament = () => {
           users={users}
         />
       </TmsModal>
-      <div className="container mx-auto mt-10 pb-20">
-        <div className="wrapper flex items-center justify-between mb-5">
-          <div className="wrapper flex items-center">
-            <Button
-              onClick={() => navigation("/admin/tournament")}
-              className="mr-5"
-            >
-              Back
-            </Button>
-            <h1 className="text-white text-3xl font-bold ">
-              {tournament?.name}
-            </h1>
-            <Badge size={"lg"} className="ml-5">
-              {tournament?.tournament_type}
-            </Badge>
+      <div className="flex">
+        <div className="basis-1/12 flex justify-start items-center flex-col py-20">
+          <div className="flex flex-col justify-center items-center my-5">
+            <FaTrophy color="white" size={35} />
+            <h1 className="text-white">Tournament</h1>
           </div>
-          <div className="wrapper flex items-center justify-center">
-            {tournament?.state == "pending" && (
-              <Tooltip content="Add Participants in the tournament">
-                <Button
-                  disabled={tournament?.state == "underway"}
-                  onClick={() => {
-                    setAddModal(true);
-                  }}
-                >
-                  Add Participants
-                </Button>
-              </Tooltip>
-            )}
-
-            {tournament?.state == "pending" && (
-              <Tooltip content="Start the tournament">
-                <Button
-                  color={"success"}
-                  className="ml-5"
-                  onClick={handleStartTournament}
-                >
-                  Start Tournament
-                </Button>
-              </Tooltip>
-            )}
-            {tournament?.state == "awaiting_review" && (
-              <Tooltip content="Finalize tournament">
-                <Button
-                  color={"success"}
-                  className="ml-5"
-                  onClick={handleFinalizeTournament}
-                >
-                  Finalize Tournament
-                </Button>
-              </Tooltip>
-            )}
+          <div className="flex flex-col justify-center items-center my-5">
+            <GiStairs color="white" size={35} />
+            <h1 className="text-white">Standing</h1>
+          </div>
+          <div className="flex flex-col justify-center items-center my-5">
+            <BsFillPeopleFill color="white" size={35} />
+            <h1 className="text-white">Participants</h1>
+          </div>
+          <div className="flex flex-col justify-center items-center my-5">
+            <TbTournament color="white" size={35} />
+            <h1 className="text-white">Matches</h1>
           </div>
         </div>
-        <div className="mb-5">
-          <Table hoverable>
-            <Table.Head>
-              <Table.HeadCell className="text-center bg-slate-900 text-white font-bold border">
-                Tournament State
-              </Table.HeadCell>
-              <Table.HeadCell className="text-center bg-slate-900 text-white font-bold border">
-                Registered Event
-              </Table.HeadCell>
-              <Table.HeadCell className="text-center bg-slate-900 text-white font-bold border">
-                Tournament Start Date
-              </Table.HeadCell>
-            </Table.Head>
-            <Table.Body className="divide-y">
-              <Table.Cell className="text-center flex items-center justify-center">
-                <Badge color={"warning"} size={"lg"} className="px-10">
-                  <h1 className="text-lg"> {tournament?.state}</h1>
+        <div className="basis-11/12 p-10">
+          <div className="container mx-auto mt-10 pb-20">
+            <div className="wrapper flex items-center justify-between mb-5">
+              <div className="wrapper flex items-center">
+                <Button
+                  onClick={() => navigation("/admin/tournament")}
+                  className="mr-5"
+                >
+                  Back
+                </Button>
+                <h1 className="text-white text-3xl font-bold ">
+                  {tournament?.name}
+                </h1>
+                <Badge size={"lg"} className="ml-5">
+                  {tournament?.tournament_type}
                 </Badge>
-              </Table.Cell>
-              <Table.Cell className="text-center">
-                <h1 className="text-lg text-white">
-                  {tournament?.description}
-                </h1>
-              </Table.Cell>
-              <Table.Cell className="text-center">
-                <h1 className="text-lg text-white">
-                  {moment(tournament?.start_at).format("LL")}
-                </h1>
-              </Table.Cell>
-            </Table.Body>
-          </Table>
-        </div>
-        <iframe
-          src={`https://challonge.com/${id}/module`}
-          width="100%"
-          height="500"
-          frameborder="0"
-          scrolling="auto"
-          allowtransparency="true"
-        ></iframe>
-        {matches.length >= 1 && (
-          <div className="matches my-20">
-            <div className="wrapper flex items-center my-5">
-              <h1 className="text-white text-3xl font-bold">
-                Tournament Matches
-              </h1>
-              <Badge className="ml-3">{matches.length}</Badge>
-            </div>
-            <div className="flex justify-start items-start flex-wrap">
-              {matches.map((item) => {
-                const { match } = item;
+              </div>
+              <div className="wrapper flex items-center justify-center">
+                {tournament?.state == "pending" && (
+                  <Tooltip content="Add Participants in the tournament">
+                    <Button
+                      disabled={tournament?.state == "underway"}
+                      onClick={() => {
+                        setAddModal(true);
+                      }}
+                    >
+                      Add Participants
+                    </Button>
+                  </Tooltip>
+                )}
 
-                return <MatchCard id={id} key={match.id} match={match} />;
-              })}
+                {tournament?.state == "pending" && (
+                  <Tooltip content="Start the tournament">
+                    <Button
+                      color={"success"}
+                      className="ml-5"
+                      onClick={handleStartTournament}
+                    >
+                      Start Tournament
+                    </Button>
+                  </Tooltip>
+                )}
+                {tournament?.state == "awaiting_review" && (
+                  <Tooltip content="Finalize tournament">
+                    <Button
+                      color={"success"}
+                      className="ml-5"
+                      onClick={handleFinalizeTournament}
+                    >
+                      Finalize Tournament
+                    </Button>
+                  </Tooltip>
+                )}
+              </div>
+            </div>
+            <div className="mb-5">
+              <Table hoverable>
+                <Table.Head>
+                  <Table.HeadCell className="text-center bg-slate-900 text-white font-bold border">
+                    Tournament State
+                  </Table.HeadCell>
+                  <Table.HeadCell className="text-center bg-slate-900 text-white font-bold border">
+                    Registered Event
+                  </Table.HeadCell>
+                  <Table.HeadCell className="text-center bg-slate-900 text-white font-bold border">
+                    Tournament Start Date
+                  </Table.HeadCell>
+                </Table.Head>
+                <Table.Body className="divide-y">
+                  <Table.Cell className="text-center flex items-center justify-center">
+                    <Badge color={"warning"} size={"lg"} className="px-10">
+                      <h1 className="text-lg"> {tournament?.state}</h1>
+                    </Badge>
+                  </Table.Cell>
+                  <Table.Cell className="text-center">
+                    <h1 className="text-lg text-white">
+                      {tournament?.description}
+                    </h1>
+                  </Table.Cell>
+                  <Table.Cell className="text-center">
+                    <h1 className="text-lg text-white">
+                      {moment(tournament?.start_at).format("LL")}
+                    </h1>
+                  </Table.Cell>
+                </Table.Body>
+              </Table>
+            </div>
+            <iframe
+              src={`https://challonge.com/${id}/module`}
+              width="100%"
+              height="500"
+              frameborder="0"
+              scrolling="auto"
+              allowtransparency="true"
+            ></iframe>
+            {matches.length >= 1 && (
+              <div className="matches my-20">
+                <div className="wrapper flex items-center my-5">
+                  <h1 className="text-white text-3xl font-bold">
+                    Tournament Matches
+                  </h1>
+                  <Badge className="ml-3">{matches.length}</Badge>
+                </div>
+                <div className="flex justify-start items-start flex-wrap">
+                  {matches.map((item) => {
+                    const { match } = item;
+
+                    return <MatchCard id={id} key={match.id} match={match} />;
+                  })}
+                </div>
+              </div>
+            )}
+
+            <div className="participants my-20">
+              <div className="wrapper flex items-center my-5">
+                <h1 className="text-white text-3xl font-bold">
+                  Tournament Participants
+                </h1>
+                <Badge className="ml-3">{participants.length}</Badge>
+              </div>
+              {participants.length <= 0 && (
+                <h1 className="text-white text-center text-3xl">
+                  No Participants yet. try adding one.
+                </h1>
+              )}
+              {participants.length >= 1 && (
+                <ParticipantsTables
+                  tournament={tournament}
+                  handleDeleteParticipant={handleDeleteParticipant}
+                  participants={participants}
+                />
+              )}
             </div>
           </div>
-        )}
-
-        <div className="participants my-20">
-          <div className="wrapper flex items-center my-5">
-            <h1 className="text-white text-3xl font-bold">
-              Tournament Participants
-            </h1>
-            <Badge className="ml-3">{participants.length}</Badge>
-          </div>
-          {participants.length <= 0 && (
-            <h1 className="text-white text-center text-3xl">
-              No Participants yet. try adding one.
-            </h1>
-          )}
-          {participants.length >= 1 && (
-            <ParticipantsTables
-              tournament={tournament}
-              handleDeleteParticipant={handleDeleteParticipant}
-              participants={participants}
-            />
-          )}
         </div>
       </div>
     </AdminLayout>
