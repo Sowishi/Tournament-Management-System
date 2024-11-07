b3e814b1200695822cb214ab755a250f897395e7
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-11-08 01:11:01 +0800

    Fix backend errors

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,31 @@ import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 5000; // Port for the backend server
-const corsOption = {
-  origin: "http://localhost:5173",
-};
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://tournament-772d1.web.app",
+];
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://tournament-772d1.web.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(cors(corsOption));
 app.use(bodyParser.json());
