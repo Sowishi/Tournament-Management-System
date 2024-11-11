372b5b15fc0feec7981a9a45ee05f2e5966d1f30
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-11-11 19:55:29 +0800

    Saved'

--- src/layout/adminLayout.jsx
+++ src/layout/adminLayout.jsx
@@ -211,6 +211,17 @@ export default function AdminLayout({ children, client }) {
                           </div>
                         </Sidebar.Item>
                       </a>
+                      <Link to="/admin/home">
+                        <Sidebar.Item
+                          onClick={() => setIsOpen(false)}
+                          className="bg-blue-950 text-white hover:text-white hover:bg-red-500 my-3"
+                        >
+                          <div className="flex items-center">
+                            Admin Dashboard
+                            <HiOutlineChartSquareBar className="ml-3" />
+                          </div>
+                        </Sidebar.Item>
+                      </Link>
                       <Link to="/admin/users">
                         <Sidebar.Item
                           onClick={() => setIsOpen(false)}
@@ -221,18 +232,7 @@ export default function AdminLayout({ children, client }) {
                           </div>
                         </Sidebar.Item>
                       </Link>
-
-                      <Link to="/admin/calendar">
-                        <Sidebar.Item
-                          onClick={() => setIsOpen(false)}
-                          className="bg-blue-950 text-white hover:text-white hover:bg-red-500 my-3"
-                        >
-                          <div className="flex items-center">
-                            Calendar <HiOutlineCalendar className="ml-3" />
-                          </div>
-                        </Sidebar.Item>
-                      </Link>
-
+                      x
                       <Sidebar.Item
                         onClick={() => setIsOpen(false)}
                         className="mt-20"
