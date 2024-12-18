e9dff271a213e25209a59c3c77c7d69b09c1e59b
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-12-19 02:47:24 +0800

    Tournament Manager fix

--- src/layout/adminLayout.jsx
+++ src/layout/adminLayout.jsx
@@ -366,7 +366,10 @@ export default function AdminLayout({ children, client }) {
                 <h1 className="text-white textt-sm md:text-2xl">
                   {currentAdmin?.role !== "Master Admin" && (
                     <>
-                      {currentAdmin?.fullName} | {currentAdmin?.sportsEvent}
+                      {currentAdmin?.fullName} |{" "}
+                      {currentAdmin?.sportsEvent
+                        ? currentAdmin?.sportsEvent
+                        : currentAdmin?.assignEvent}
                     </>
                   )}
                 </h1>
